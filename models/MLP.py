533732f24e62738a13af20cb295be0c09640ae8f
Matthew O <mathonich@yahoo.com>
2025-05-27 17:43:36 -0700

    MLP w/ tuneable num layers

--- models/MLP.py
+++ models/MLP.py
@@ -4,27 +4,28 @@ from torch import nn
 class MLP(nn.Module):
     def __init__(self, *args) -> None:
         super().__init__()
+
         kwargs = args[0]
-        input_dim = kwargs.get("input_dim", 28 * 28)
-        hidden_dim = kwargs.get("hidden_dim", 56)
-        output_dim = kwargs.get("output_dim", 10)
+        input_dim = kwargs.get("input_dim")
+        hidden_dim = kwargs.get("hidden_dim")
+        output_dim = kwargs.get("output_dim")
+        num_hidden_layers = kwargs.get("num_hidden_layers")
 
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
 
-        self.fc1 = nn.Sequential(
-            nn.Linear(self.input_dim, self.hidden_dim),
-            nn.ReLU(),
-        )
-        # self.fc2 = nn.Sequential(
-        #     nn.Linear(self.hidden_dim, self.hidden_dim),
-        #     nn.ReLU(),
-        # )
-        self.fc3 = nn.Linear(self.hidden_dim, self.output_dim)
+        layers = []
+        dim = input_dim
+
+        for _ in range(num_hidden_layers):
+            layers.append(nn.Linear(dim, self.hidden_dim))
+            layers.append(nn.ReLU())
+            dim = self.hidden_dim
+
+        layers.append(nn.Linear(self.hidden_dim, self.output_dim))
+
+        self.net = nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.fc1(x)
-        # x = self.fc2(x)
-        x = self.fc3(x)
-        return x
+        return self.net(x)
